30c62b0e8fd918b774087359908ec9d63461edf0
sreekel <50909565+sreeesjlqkjfkqjsdflkqjsflqjsdflqjf@users.noreply.github.com>
2025-04-01 15:13:02 +0200

    nettoyage

--- Readme.md
+++ Readme.md
@@ -1,2 +1,11 @@
 ### Localisation bluetooth
+A simple script using the averaged RSSI of bluetooth beacon to give the position of the receiver. 
+The Log-distance path loss model is used ([wiki](https://en.wikipedia.org/wiki/Log-distance_path_loss_model)):
 
+![equation](https://latex.codecogs.com/svg.image?L=L_\text{Tx}-L_\text{Rx}=L_0&plus;10\gamma\log_{10}\frac{d}{d_0}&plus;X_\text{g})  
+
+The RSSI measurement is made with [bluepy](https://pypi.org/project/bluepy/).
+
+Only one RSSI measurement per second is possible (but every beacon at the same time) so the averaging require a bit of time.
+
+![](https://github.com/sreeesjlqkjfkqjsdflkqjsflqjsdflqjf/blueloc/blob/master/image/illustration.png)
--- main.py
+++ main.py
@@ -8,9 +8,7 @@ from tabulate import tabulate
 import time
 import numpy as np
 import json
-nb_scan = 10
-pair_valeur_addr = []
-valeurs = []
+nb_scan = 20
 scanner = Scanner()
 class Lardon:
     def __init__(self, x, y, addr):
@@ -21,6 +19,8 @@ class Lardon:
         self.y = y
         self.addr = addr
     def distance(self):
+        if self.mesures==[]:
+            return self.dist
         avg = sum(self.mesures)/len(self.mesures)
         d = pow(10, abs(avg+58)/(10*2))
         return d
@@ -30,19 +30,21 @@ class LardonSauvegarde(json.JSONEncoder):
         if isinstance(o, Lardon):
             return ["__lardon__", o.addr, o.x, o.y]
         return super().default(o)
-    def charge(self, l):
-        return Lardon(l[1], int(l[2]), int(l[3]))
-# lardons = [Lardon(220, 183, "88:6b:0f:a2:ac:e8"), Lardon(270, 243, "88:6b:0f:a2:b0:31")] 
-lardons = []
+class LardonCharge(json.JSONDecoder):
+
+    def decode(self, s, _w=""):
+        obj, _ = self.raw_decode(s)
+        obj = [Lardon(int(l[2]), int(l[3]), l[1]) for l in obj]
+        return obj
 def decouverte():
     scan = scanner.scan(timeout=1)
     arr = []
     for s in scan:
-        if str(s.addr)[:5]=="88:6b" or True:
+        if str(s.addr)[:5]=="88:6b":
             arr.append([s.addr, s.rssi])
     print(tabulate(arr, headers = ["addresse", "rssi"], tablefmt="heavy_grid"))
     return arr
-sur_clique_arr = ["lol"]
+sur_clique_arr = []
 def sur_clique(event):
     try:
         if event.button is MouseButton.LEFT:
@@ -50,22 +52,22 @@ def sur_clique(event):
             lardons.append(Lardon(event.x, event.y, dev[0]))
             print(event.x, event.y)
     except:
-        print(lardons)
         config_sauv()
         plt.close()
 def config_init():
     global sur_clique_arr
     sur_clique_arr = decouverte()
     fig, ax = plt.subplots()
-    ax.imshow(mpimg.imread("./Screenshot_2024-11-12_12-04-42.png"))
+    ax.imshow(mpimg.imread("./image/plan.png"))
     plt.connect('button_press_event', sur_clique)
     plt.show()
 def config_sauv():
     with open("config.json", "w") as f:
         json.dump(lardons, f, cls=LardonSauvegarde)
 def charge_config():
+    global lardons
     with open("config.json", "r") as f:
-        lardons = json.load(f)
+        lardons = json.load(f, cls=LardonCharge)
 def fait_un_point():
     for n in range(nb_scan):
         scan = scanner.scan(timeout=1)
@@ -85,12 +87,11 @@ def affiche():
         c = mpatches.Circle((l.x, l.y), echelle * l.distance(), color='r', fill=False)
         ax.add_patch(c)
     plt.show()
-
+# lardons = [Lardon(222, 703, "88:6b:0f:a2:b0:31"), Lardon(225, 826, "88:6b:0f:a2:ac:e8"), Lardon(278, 755, "88:6b:0f:a2:b0:88")]
 if __name__ == "__main__":
     début = time.time()
-    charge_config()
-    config_init()
-    # decouverte()
-    fait_un_point()
+    # charge_config()
+    # config_init()
+    # fait_un_point()
     print(f"{time.time()-début} secondes")
     affiche()
